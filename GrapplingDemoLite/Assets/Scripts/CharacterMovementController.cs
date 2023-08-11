abb02127af8bb480010a2375e6c6c51e151b721c
Bangyu Li <by.brad.li@gmail.com>
2023-08-12 05:46:50 +0800

    Fix player movement direction

--- GrapplingDemoLite/Assets/Scripts/CharacterMovementController.cs
+++ GrapplingDemoLite/Assets/Scripts/CharacterMovementController.cs
@@ -8,7 +8,18 @@ public class CharacterMovementController : MonoBehaviour
     public float moveSpeed = 5f;
     public float runSpeed = 10f;
 
+    public float RotationSmoothTime = 0.12f;
+    private float _targetRotation = 0.0f;
+    private float _rotationVelocity;
+
     private Vector3 moveDirection;
+
+    CharacterController controller;
+
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
 
     void Update()
     {
@@ -16,6 +27,24 @@ public class CharacterMovementController : MonoBehaviour
         float z = Input.GetAxisRaw("Vertical");
 
         moveDirection = new Vector3(x, 0, z).normalized;
+
+        Debug.Log(controller.velocity);
+        if (controller.velocity != Vector3.zero)
+        {
+            _targetRotation = Mathf.Atan2(moveDirection.x, moveDirection.z) * Mathf.Rad2Deg +
+                              Camera.main.transform.eulerAngles.y;
+            float rotation = Mathf.SmoothDampAngle(transform.eulerAngles.y, _targetRotation, ref _rotationVelocity,
+                RotationSmoothTime);
+
+            // rotate to face input direction relative to camera position
+            transform.rotation = Quaternion.Euler(0.0f, rotation, 0.0f);
+        }
+
+        Vector3 targetDirection = Vector3.zero;
+        if (moveDirection != Vector3.zero)
+        {
+            targetDirection = Quaternion.Euler(0.0f, _targetRotation, 0.0f) * Vector3.forward;
+        }
 
         if (Input.GetKey(KeyCode.LeftShift))
         {
@@ -26,9 +55,6 @@ public class CharacterMovementController : MonoBehaviour
             currentSpeed = moveSpeed;
         }
 
-        Vector3 velocity = moveDirection * currentSpeed * Time.deltaTime;
-
-        transform.position += velocity;
+        controller.Move(targetDirection.normalized * (currentSpeed * Time.deltaTime));
     }
-
 }
